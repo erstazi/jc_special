6344e8ff92f260adcf08926c71ac18fe9e29f101
erstazi <erstazi@gmail.com>
2026-08-14 01:43:58 -0400

    cleaning up some code with init.lua

--- init.lua
+++ init.lua
@@ -1,23 +1,9 @@
-core.log("action", "[jc_special] INIT STARTED")
-
 local S = core.get_translator(core.get_current_modname())
 local modpath = core.get_modpath(core.get_current_modname())
-
-core.log("action", "[jc_special] modpath = " .. tostring(modpath))
 
 dofile(modpath .. "/daybutton.lua")
 dofile(modpath .. "/welcomeplayers.lua")
 dofile(modpath .. "/snowball.lua")
-
-core.log("action", "[jc_special] reached homedecor check")
-
--- if core.get_modpath("homedecor") then
-  core.log("action", "[jc_special] homedecor FOUND")
-  dofile(modpath .. "/homedecor_overrides.lua")
-  core.log("action", "[jc_special] homedecor overrides should have ran")
--- else
-  -- core.log("action", "[jc_special] homedecor NOT FOUND")
--- end
-
+dofile(modpath .. "/homedecor_overrides.lua")
 dofile(modpath .. "/animalworld_cleanup.lua")
 dofile(modpath .. "/misc_overrides.lua")

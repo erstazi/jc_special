7b0f373266b57cb4c7075a587c8ee2765cee54a2
erstazi <erstazi@gmail.com>
2026-09-13 21:22:00 -0400

    Updating the way translations are handled for Stadium Seats and Dugout Seats

--- stadium_seats.lua
+++ stadium_seats.lua
@@ -26,6 +26,10 @@ local function stadium_seat_rightclick(pos, node, clicker, itemstack, pointed_th
   local is_attached = clicker:get_attach()
   local name = clicker:get_player_name()
 
+  if not core.settings:get_bool("jc_special_stadium_seat_sounds", true) then
+    return result
+  end
+
   if not was_attached and is_attached then
     if cushion_alt_players[name] then
       cushion_cycles[name] = (cushion_cycles[name] or 0) + 1
@@ -61,30 +65,30 @@ local function stadium_seat_rightclick(pos, node, clicker, itemstack, pointed_th
   return result
 end
 
-local colors_table = {
-  {"white",      S("White") },
-  {"grey",       S("Grey") },
-  {"dark_grey",  S("Dark Grey") },
-  {"black",      S("Black") },
-  {"violet",     S("Violet") },
-  {"blue",       S("Blue") },
-  {"cyan",       S("Cyan") },
-  {"dark_green", S("Dark Green") },
-  {"green",      S("Green") },
-  {"yellow",     S("Yellow") },
-  {"brown",      S("Brown") },
-  {"orange",     S("Orange") },
-  {"red",        S("Red") },
-  {"magenta",    S("Magenta") },
-  {"pink",       S("Pink") },
+local colors_table_stadium_seat_general = {
+  {"white",      S("General Stadium Seat (White)") },
+  {"grey",       S("General Stadium Seat (Grey)") },
+  {"dark_grey",  S("General Stadium Seat (Dark Grey)") },
+  {"black",      S("General Stadium Seat (Black)") },
+  {"violet",     S("General Stadium Seat (Violet)") },
+  {"blue",       S("General Stadium Seat (Blue)") },
+  {"cyan",       S("General Stadium Seat (Cyan)") },
+  {"dark_green", S("General Stadium Seat (Dark Green)") },
+  {"green",      S("General Stadium Seat (Green)") },
+  {"yellow",     S("General Stadium Seat (Yellow)") },
+  {"brown",      S("General Stadium Seat (Brown)") },
+  {"orange",     S("General Stadium Seat (Orange)") },
+  {"red",        S("General Stadium Seat (Red)") },
+  {"magenta",    S("General Stadium Seat (Magenta)") },
+  {"pink",       S("General Stadium Seat (Pink)") },
 }
 
-for _, dye in ipairs(colors_table) do
-  local color = dye[1]
-  local color_name = dye[2]
+for _, color_item in ipairs(colors_table_stadium_seat_general) do
+  local color = color_item[1]
+  local description_name = color_item[2]
 
   core.register_node("jc_special:stadium_seat_general_" .. color, {
-    description = S("General Stadium Seat (@1)", color_name),
+    description = description_name,
     drawtype = "mesh",
     mesh = "stadium_seat.obj",
     tiles = {
@@ -113,12 +117,30 @@ for _, dye in ipairs(colors_table) do
   })
 end
 
-for _, dye in ipairs(colors_table) do
-  local color = dye[1]
-  local color_name = dye[2]
+local colors_table_dugout_seat = {
+  {"white",      S("Dugout Seat (White)") },
+  {"grey",       S("Dugout Seat (Grey)") },
+  {"dark_grey",  S("Dugout Seat (Dark Grey)") },
+  {"black",      S("Dugout Seat (Black)") },
+  {"violet",     S("Dugout Seat (Violet)") },
+  {"blue",       S("Dugout Seat (Blue)") },
+  {"cyan",       S("Dugout Seat (Cyan)") },
+  {"dark_green", S("Dugout Seat (Dark Green)") },
+  {"green",      S("Dugout Seat (Green)") },
+  {"yellow",     S("Dugout Seat (Yellow)") },
+  {"brown",      S("Dugout Seat (Brown)") },
+  {"orange",     S("Dugout Seat (Orange)") },
+  {"red",        S("Dugout Seat (Red)") },
+  {"magenta",    S("Dugout Seat (Magenta)") },
+  {"pink",       S("Dugout Seat (Pink)") },
+}
+
+for _, color_item in ipairs(colors_table_dugout_seat) do
+  local color = color_item[1]
+  local description_name = color_item[2]
 
   core.register_node("jc_special:dugout_seat_" .. color, {
-    description = S("Dugout Seat (@1)", color_name),
+    description = description_name,
     drawtype = "mesh",
     mesh = "dugout_seat.obj",
     tiles = {

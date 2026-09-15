3fe0f95d316ce0b2227d709c2553434cad3560a5
erstazi <erstazi@gmail.com>
2026-09-14 22:40:58 -0400

    Ugh

--- mobs_monsters.lua
+++ mobs_monsters.lua
@@ -555,7 +555,7 @@ function Monster:patrol(self, dtime, def)
   -- exactly where it left off.
   --------------------------------------------------------------
   if patrol.look_at_players then
-    local player = Monster:find_visible_player( self, patrol.detection_radius or 20, detection.see_through_nodes or false )
+    local player = Monster:find_visible_player( self, patrol.detection_radius or 20, false )
 
     if player then
 
